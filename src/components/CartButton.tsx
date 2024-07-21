820c91c29a1c4d20af1a1d59fb51e2d97cf293a6
MTayeb93 <m.tayeb993@gmail.com>
2024-07-21 11:47:12 +0200

    wrap up navbar component

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -1,10 +1,24 @@
+import { Button } from './ui/button';
+import { ShoppingCart } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
-const CartButton = () => {
+function CartButton() {
+  const numItemsInCart = 5;
+
   return (
-    <div>
-      CartButton
-    </div>
-  )
+    <Button
+      asChild
+      variant='outline'
+      size='icon'
+      className='relative flex items-center justify-center'
+    >
+      <Link to='/cart'>
+        <ShoppingCart />
+        <span className='absolute flex items-center justify-center w-6 h-6 text-xs text-white rounded-full -top-3 -right-3 bg-primary'>
+          {numItemsInCart}
+        </span>
+      </Link>
+    </Button>
+  );
 }
-
-export default CartButton
+export default CartButton;
--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,10 +1,25 @@
+import { links } from '@/utils';
+import { NavLink } from 'react-router-dom';
 
-const NavLinks = () => {
+function NavLinks() {
   return (
-    <div>
-      NavLinks
+    <div className='items-center justify-center hidden lg:flex gap-x-4'>
+      {links.map((link) => {
+        return (
+          <NavLink
+            to={link.href}
+            key={link.label}
+            className={({ isActive }) => {
+              return `capitalize font-light tracking-wide ${
+                isActive ? 'text-primary' : ''
+              }`;
+            }}
+          >
+            {link.label}
+          </NavLink>
+        );
+      })}
     </div>
-  )
+  );
 }
-
-export default NavLinks
+export default NavLinks;
