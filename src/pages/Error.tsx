215471c76d9615209b3c0e2fb5d265cd2b5824b5
MTayeb93 <m.tayeb993@gmail.com>
2024-07-21 12:04:44 +0200

    tackle error page / error element

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,10 +1,35 @@
+import { useRouteError, Link, isRouteErrorResponse } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
 
 const Error = () => {
-  return (
-    <div>
-      Error
-    </div>
-  )
-}
+  // type:unknown
+  const error = useRouteError();
+  console.log(error);
+  if (isRouteErrorResponse(error) && error.status === 404) {
+    return (
+      <main className='grid min-h-[100vh] place-items-center px-8 '>
+        <div className='text-center'>
+          <p className='font-semibold text-9xl text-primary'>404</p>
+          <h1 className='mt-4 text-3xl font-bold tracking-tight sm:text-5xl'>
+            Page not found
+          </h1>
+          <p className='mt-6 text-lg leading-7'>
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+          <div className='mt-10 '>
+            <Button asChild size='lg' variant='secondary'>
+              <Link to='/'>Go back home</Link>
+            </Button>
+          </div>
+        </div>
+      </main>
+    );
+  }
 
-export default Error
+  return (
+    <main className='grid min-h-[100vh] place-items-center px-8 '>
+      <h4 className='text-4xl font-bold text-center'>there was an error... </h4>
+    </main>
+  );
+};
+export default Error;
