8b2206a04f8bcb5874a9a168a44a79513ad60714
MTayeb93 <m.tayeb993@gmail.com>
2024-07-30 11:56:04 +0200

    progress on cart page/slice

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,18 +1,42 @@
-import { Link } from "react-router-dom"
-import { Button } from "@/components/ui/button"
+import { useAppSelector } from '@/hooks';
+import SectionTitle from '@/components/ui/SectionTitle';
+import CartItemsList from '@/components/cart/CartItemsList';
+import CartTotals from '@/components/cart/CartTotals';
+import { Link } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
 
 const Cart = () => {
-  return (
-    <div>
-      <h1 className="text-8xl">Cart Page</h1>
-      <Link to='/' className='text-red-900 text-7xl'>
-        back home
-      </Link>
-      <Button asChild size='lg'>
-        <Link to='/'>home button</Link>
-      </Button>
-    </div>
-  )
-}
+  // temp
+  const user = null;
 
-export default Cart
+  const numItemsInCart = useAppSelector(
+    (state) => state.cartState.numItemsInCart
+  );
+
+  if (numItemsInCart === 0) {
+    return <SectionTitle text='Empty cart ☹️' />;
+  }
+  return (
+    <>
+      <SectionTitle text='Shopping Cart' />
+      <div className='grid gap-8 mt-8 lg:grid-cols-12'>
+        <div className='lg:col-span-8'>
+          <CartItemsList />
+        </div>
+        <div className='lg:col-span-4 lg:pl-4'>
+          <CartTotals />
+          {user ? (
+            <Button asChild className='w-full mt-8'>
+              <Link to='/checkout'>Proceed to checkout</Link>
+            </Button>
+          ) : (
+            <Button asChild className='w-full mt-8'>
+              <Link to='/login'>Please Login</Link>
+            </Button>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+export default Cart;
--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -1,74 +1,103 @@
-import { useLoaderData } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import {
+  CartItem,
   customFetch,
   formatAsDollars,
   type SingleProductResponse,
-} from '@/utils';
-import { useState } from 'react';
-import { Button } from '@/components/ui/button';
-import { Separator } from '@/components/ui/separator';
+} from '@/utils'
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
 
-import { type LoaderFunction } from 'react-router-dom';
+import { type LoaderFunction } from 'react-router-dom'
+import SelectProductColor from '@/components/product/SelectProductColor'
+import SelectProductAmount from '@/components/product/SelectProductAmount'
+import { Mode } from '@/components/product/SelectProductAmount';
+import { useAppDispatch } from '@/hooks'
+import { addItem } from '@/features/cart/cartSlice'
 
 export const loader: LoaderFunction = async ({
   params,
 }): Promise<SingleProductResponse> => {
   const response = await customFetch<SingleProductResponse>(
     `/products/${params.id}`
-  );
+  )
 
-  return { ...response.data };
-};
+  return { ...response.data }
+}
 
 const SingleProduct = () => {
-  const { data: product } = useLoaderData() as SingleProductResponse;
+  const { data: product } = useLoaderData() as SingleProductResponse
   const { image, title, price, description, colors, company } =
-    product.attributes;
-  const dollarsAmount = formatAsDollars(price);
-  const [productColor, setProductColor] = useState(colors[0]);
-  const [amount, setAmount] = useState(1);
+    product.attributes
+  const dollarsAmount = formatAsDollars(price)
+  const [productColor, setProductColor] = useState(colors[0])
+  const [amount, setAmount] = useState(1)
 
+  const dispatch = useAppDispatch();
+
+  const cartProduct: CartItem = {
+    cartID: product.id + productColor,
+    productID: product.id,
+    image,
+    title,
+    price,
+    amount,
+    productColor,
+    company,
+  };
+  
   const addToCart = () => {
-    console.log('add to cart');
+    dispatch(addItem(cartProduct));
   };
 
-  return <section>
-  <div className='flex items-center h-6 gap-x-2'>
-    <Button asChild variant='link' size='sm'>
-      <Link to='/'>Home</Link>
-    </Button>
-    <Separator orientation='vertical' />
-    <Button asChild variant='link' size='sm'>
-      <Link to='/products'>Products</Link>
-    </Button>
-  </div>
-  {/* PRODUCT */}
-  <div className='grid mt-6 gap-y-8 lg:grid-cols-2 lg:gap-x-16'>
-    {/* IMAGE */}
-    <img
-      src={image}
-      alt={title}
-      className='object-cover rounded-lg w-96 h-96 lg:w-full'
-    />
-    {/* PRODUCT INFO */}
-    <div>
-      <h1 className='text-3xl font-bold capitalize'>{title}</h1>
-      <h4 className='mt-2 text-xl'>{company}</h4>
-      <p className='inline-block p-2 mt-3 rounded-md text-md bg-muted'>
-        {dollarsAmount}
-      </p>
-      <p className='mt-6 leading-8'>{description}</p>
-      {/* COLORS */}
-
-      {/* AMOUNT */}
-
-      {/* CART BUTTON */}
-      <Button size='lg' className='mt-10' onClick={addToCart}>
-        Add to bag
-      </Button>
-    </div>
-  </div>
-</section>;
-};
-export default SingleProduct;
+  return (
+    <section>
+      <div className='flex items-center h-6 gap-x-2'>
+        <Button asChild variant='link' size='sm'>
+          <Link to='/'>Home</Link>
+        </Button>
+        <Separator orientation='vertical' />
+        <Button asChild variant='link' size='sm'>
+          <Link to='/products'>Products</Link>
+        </Button>
+      </div>
+      {/* PRODUCT */}
+      <div className='grid mt-6 gap-y-8 lg:grid-cols-2 lg:gap-x-16'>
+        {/* IMAGE */}
+        <img
+          src={image}
+          alt={title}
+          className='object-cover rounded-lg w-96 h-96 lg:w-full'
+        />
+        {/* PRODUCT INFO */}
+        <div>
+          <h1 className='text-3xl font-bold capitalize'>{title}</h1>
+          <h4 className='mt-2 text-xl'>{company}</h4>
+          <p className='inline-block p-2 mt-3 rounded-md text-md bg-muted'>
+            {dollarsAmount}
+          </p>
+          <p className='mt-6 leading-8'>{description}</p>
+          {/* COLORS */}
+          <SelectProductColor
+            colors={colors}
+            productColor={productColor}
+            setProductColor={setProductColor}
+          />
+          {/* AMOUNT */}
+          <SelectProductAmount
+            mode={Mode.SingleProduct}
+            amount={amount}
+            setAmount={setAmount}
+          />
+          {/* CART BUTTON */}
+          <Button size='lg' className='mt-10' onClick={addToCart}>
+            Add to bag
+          </Button>
+        </div>
+      </div>
+    </section>
+  )
+}
+export default SingleProduct
